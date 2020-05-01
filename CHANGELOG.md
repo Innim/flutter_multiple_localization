d998d70ef2ae0e84eee3f9a29bc539ecf69cac80
greymag <greymag@gmail.com>
2020-05-01 19:48:51 +0300

    Refactor example project. All not generated code in one file now.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.1.0+3] - 2020-05-01
+
+- Refactor example project. All not generated code in one file now.
+
 ## [0.1.0+2] - 2020-05-01
 
 - Example project.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:multiple_localization_example/app/app_localization.dart';
-import 'package:multiple_localization_example/package/package_localization.dart';
+import 'package:intl/intl.dart';
+import 'package:multiple_localization/multiple_localization.dart';
+import './app/l10n/messages_all.dart';
+import './package/l10n/messages_all.dart' as package;
 
 void main() async {
   runApp(
@@ -40,4 +42,87 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+// Application localization
+
+class _AppLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizations> {
+  const _AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => ['en'].contains(locale.languageCode);
+
+  @override
+  Future<AppLocalizations> load(Locale locale) {
+    return MultipleLocalizations.load(
+        initializeMessages, locale, (l) => AppLocalizations(l),
+        setDefaultLocale: true);
+  }
+
+  @override
+  bool shouldReload(LocalizationsDelegate<AppLocalizations> old) => false;
+}
+
+/// App localization.
+class AppLocalizations {
+  /// Delegate.
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+      _AppLocalizationsDelegate();
+
+  static AppLocalizations of(BuildContext context) =>
+      Localizations.of<AppLocalizations>(context, AppLocalizations);
+
+  final String locale;
+
+  AppLocalizations(this.locale) : assert(locale != null);
+
+  String get title => Intl.message('Multiple localization', name: 'title');
+
+  String get messageFromApp =>
+      Intl.message('Default Message from App', name: 'messageFromApp');
+
+  String get messageFromPackageForOverride =>
+      Intl.message('Default overrided message from Package',
+          name: 'messageFromPackageForOverride');
+}
+
+// Other localization, for example from package
+
+class _PackageLocalizationsDelegate
+    extends LocalizationsDelegate<PackageLocalizations> {
+  const _PackageLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => ['en'].contains(locale.languageCode);
+
+  @override
+  Future<PackageLocalizations> load(Locale locale) {
+    return MultipleLocalizations.load(
+        package.initializeMessages, locale, (l) => PackageLocalizations(l));
+  }
+
+  @override
+  bool shouldReload(LocalizationsDelegate<PackageLocalizations> old) => false;
+}
+
+/// Package localization.
+class PackageLocalizations {
+  /// Delegate.
+  static const LocalizationsDelegate<PackageLocalizations> delegate =
+      _PackageLocalizationsDelegate();
+
+  static PackageLocalizations of(BuildContext context) =>
+      Localizations.of<PackageLocalizations>(context, PackageLocalizations);
+
+  final String locale;
+
+  PackageLocalizations(this.locale) : assert(locale != null);
+
+  String get messageFromPackage =>
+      Intl.message('Default Message from Package', name: 'messageFromPackage');
+
+  String get messageFromPackageForOverride =>
+      Intl.message('Default Message from Package for override',
+          name: 'messageFromPackageForOverride');
 }
